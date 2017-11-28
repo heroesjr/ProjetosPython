03b6c8f292dd23c849a94705d8885ce41403b3d2
Luiz Torres Junior <luiztrjunior@hotmail.com>
2017-11-28 12:09:54 -0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 from collections import Counter
-arq = open('C:/Users/luiz.junior/Desktop/ocr.txt', 'r')
+arq = open('caminho_geral_do_arquivo', 'r')
 texto_longo = arq.read()
 palavras = texto_longo
 contador = Counter(palavras)
